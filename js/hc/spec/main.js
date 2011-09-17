af902bb7c1783c20bf7009fbb3127b7a54a49ed3
Brian Cavalier <brian@briancavalier.com>
2011-09-17 14:02:34 -0400

    Comments comments comments!

--- js/hc/spec/main.js
+++ js/hc/spec/main.js
@@ -1,40 +1,61 @@
 define({
+	// wire plugins
+	// wire will detect these no matter where you put them
+	// An array just seems convenient
 	plugins: [
+		// debug firehose
 //		{ module: 'wire/debug' },
 		{ module: 'wire/dom', classes: { init: 'loading', ready: 'notes-ready' } },
+		// Use dojo.connect for event connections
 		{ module: 'wire/dojo/events' },
+		// Pull in dojo/store features, like the resource! resolver
 		{ module: 'wire/dojo/store' },
+		// Use dijit plugin to auto-theme
 		{ module: 'hc/wire/dijit', theme: 'claro' },
+		// Let's do some AOP!
 		{
 			module: 'wire/aop',
 			aspects: [ 'storeAdvice' ]
 		}
 	],
+	//
+	// Create the app controller
 	appController: {
 		create: 'hc/controller/AppController',
 		properties: {
+			// Whoah! inject a function that will launch
+			// the User Prefs area from another wire spec!
 			_showPrefs: { wire: { spec: 'hc/spec/prefs', defer: true } }
 		},
 		connect: {
+			// dojo.connect headerView's onPrefs event to
+			// appControllers's showPrefs function
 			headerView: {
 				onPrefs:   'showPrefs'
 			}
 		}
 	},
+	//
+	// Header
 	headerView: {
+		// Create a dijit-based view on the #main-header node
 		create: {
 			module: 'hc/view/headerView/HeaderView',
 			args: [{}, { $ref: 'dom!main-header' }]
 		}
 	},
+	//
+	// Notes controller
 	noteController: {
 		create: 'hc/controller/NotesController',
+		// Inject references to views and the notes store
 		properties: {
 			_noteView: { $ref: 'noteView' },
 			_noteListView: { $ref: 'noteListView' },
 			_notesStore: { $ref: 'notesStore' }
 		},
 		connect: {
+			// Connect view events
 			headerView: {
 				onNewNote: '_createNewNote'
 			},
@@ -47,6 +68,9 @@ define({
 		},
 		init: 'init'
 	},
+	//
+	// Create a JsonRest store for the notes, and
+	// make it Observable!
 	notesStore: {
 		create: {
 			module: 'dojo/store/JsonRest',
@@ -56,15 +80,24 @@ define({
 			observable: []
 		}
 	},
+	//
+	// AOP aspect that will be applied by wire/aop plugin
 	storeAdvice: { create: 'hc/aspects/AdaptStoreQueries' },
+	//
+	// dojo/store/Observable decorator
 	observable: { module: 'dojo/store/Observable' },
+	//
+	// List of notes
 	noteListView: {
 		create: {
 			module: 'hc/view/noteListView/NoteListView',
 			args: {}
 		},
+		// The hc/wire/dijit plugin adds the placeAt
+		// feature to wire for dijits
 		placeAt: { $ref: 'dom!note-list' }
 	},
+	// The note editor view
 	noteView: {
 		create: {
 			module: 'hc/view/noteView/NoteView',
--- js/hc/spec/prefs.js
+++ js/hc/spec/prefs.js
@@ -1,4 +1,5 @@
 define({
+	// wire plugins
 	plugins: [
 //		{ module: 'wire/debug' },
 		{ module: 'wire/dom', classes: { init: 'prefs-loading', ready: 'prefs-ready'} },
@@ -10,28 +11,47 @@ define({
 			aspects: [ 'storeAdvice' ]
 		}
 	],
+	// Jobs reference data.
+	// Since we're injecting into a dijit, we need to use
+	// a dojo/data type store, or an ObjectStore adapter.
+	// The hc/wire/dijit plugin provides a datastore!
+	// resolver for that.
 	jobsStore: { $ref: 'datastore!js/data/jobs/index.json' },
+	//
+	// The main prefs view
 	view: {
 		create: {
 			module: 'hc/view/prefsView/PrefsView',
+			// We can inject dependencies via constructor args, too!
 			args: {
 				jobsStore: { $ref: 'jobsStore' },
 				value: { $ref: 'resource!js/data/prefs/', get: 1 }
 			}
 		},
+		// Again, the placeAt feature is provided by the
+		// hc/wire/dijit plugin
 		placeAt: { $ref: 'dom!dialog-container' },
+		// During wire's "initialize" phase, call the
+		// view's focus() method
 		init: 'focus'
 	},
+	//
+	// Prefs controller
 	controller: {
 		create: 'hc/controller/PrefsController',
+		// Inject references to the view and a prefs store
 		properties: {
 			_prefsStore: { $ref: 'resource!js/data/prefs' },
 			_prefsView:  { $ref: 'view' }
 		},
 		connect: {
+			// Connect view events.
 			view: {
 				onSave: '_savePrefs'
 			},
+			// Also connect back to appController.  Could
+			// use pubsub here via the wire/dojo/pubsub
+			// connector, too
 			prefsSaved: {
 				appController: 'showNotes'
 			}
